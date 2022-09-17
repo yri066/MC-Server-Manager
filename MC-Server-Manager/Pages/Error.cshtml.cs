43f38358dbba2d22a5944d955f90c80dda5b9e78
yri066 <yri066.ru@gmail.com>
2022-09-17 17:10:59 +0300

    Изменил параметры и пространство имен

--- MC-Server-Manager/Pages/Error.cshtml.cs
+++ MC-Server-Manager/Pages/Error.cshtml.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Diagnostics;
 
-namespace MC_Server_Manager.Pages
+namespace MCServerManager.Pages
 {
 	[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
 	[IgnoreAntiforgeryToken]
