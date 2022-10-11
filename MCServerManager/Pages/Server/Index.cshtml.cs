ba836fe4606842359c30c53afc257adab4b87347
yri066 <113674156+Daria-Zagoruyko@users.noreply.github.com>
2022-10-11 14:47:45 +0300

    Добавила страницу с информацией о сервере

---
+++ MCServerManager/Pages/Server/Index.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MCServerManager.Pages.Server
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
