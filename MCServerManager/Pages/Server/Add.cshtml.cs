c95e9390da2ca18862b47545aaf30a03bfd57b4b
yri066 <yri066.ru@gmail.com>
2022-10-13 12:05:35 +0300

    Реализовал обработку запросов

--- MCServerManager/Pages/Server/Add.cshtml.cs
+++ MCServerManager/Pages/Server/Add.cshtml.cs
@@ -1,3 +1,5 @@
+using MCServerManager.Models;
+using MCServerManager.Service;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +7,39 @@ namespace MCServerManager.Pages.Server
 {
 	public class AddModel : PageModel
 	{
+		[BindProperty]
+		public ServerDetail Input { get; set; }
+		private readonly ServerService _service;
+
+		public AddModel(ServerService service)
+		{
+			_service = service;
+		}
 		public void OnGet()
 		{
+			Input = new();
+		}
+
+		public IActionResult OnPost()
+		{
+			try
+			{
+				if (ModelState.IsValid)
+				{
+					var id = _service.CreateServer(Input.Name, Input.AutoStart, Input.WorkDirectory, Input.Programm,
+						Input.Arguments, Input.Addres, Input.Port);
+					return RedirectToPage("Index", new { id });
+				}
+			}
+			catch (Exception ex)
+			{
+				ModelState.AddModelError(
+					string.Empty,
+					ex.Message
+					);
+			}
+
+			return Page();
 		}
 	}
 }
