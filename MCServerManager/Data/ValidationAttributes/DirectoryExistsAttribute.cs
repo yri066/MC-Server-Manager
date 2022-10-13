c52273c80133daeb632f2a1b3eddcefcebc499de
yri066 <yri066.ru@gmail.com>
2022-10-13 12:01:00 +0300

    Добавил атрибут, проверяющий наличие указанной директории

---
+++ MCServerManager/Data/ValidationAttributes/DirectoryExistsAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MCServerManager.Data.ValidationAttributes
+{
+	public class DirectoryExistsAttribute : ValidationAttribute
+	{
+		public string GetErrorMessage() => "Указанная директория не найдена";
+
+		protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+		{
+			if (!Directory.Exists((string)value!))
+			{
+				return new ValidationResult(GetErrorMessage());
+			}
+
+			return ValidationResult.Success;
+		}
+	}
+}
