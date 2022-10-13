dcecbd2b464eec876c722fff9dfea4d67e914042
yri066 <yri066.ru@gmail.com>
2022-10-13 12:14:11 +0300

    Добавил получение информации о сервере и контроллер удаление сервера из списка серверов

--- MCServerManager/Pages/Server/Index.cshtml.cs
+++ MCServerManager/Pages/Server/Index.cshtml.cs
@@ -2,15 +2,47 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MCServerManager.Library.Actions;
+using MCServerManager.Service;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace MCServerManager.Pages.Server
 {
-    public class IndexModel : PageModel
-    {
-        public void OnGet()
-        {
-        }
-    }
+	public class IndexModel : PageModel
+	{
+		private readonly ServerService _serverService;
+		public GameServer Exemplar { get; private set; }
+
+		public IndexModel(ServerService serverService)
+		{
+			_serverService = serverService;
+		}
+
+		public IActionResult OnGet(Guid id)
+		{
+			try
+			{
+				Exemplar = _serverService.GetServer(id);
+			}
+			catch
+			{
+				return NotFound();
+			}
+
+			return Page();
+		}
+
+		public IActionResult OnGetDelete(Guid id)
+		{
+			try
+			{
+				_serverService.DeleteServer(id);
+			}
+			catch
+			{ }
+
+			return RedirectToPage("List");
+		}
+	}
 }
